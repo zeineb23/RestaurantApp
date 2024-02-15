ac305f83afd68f7ae41e4c736dc00e3d72d36786
Seyfeddine Jouini <jouini.seyf.js@gmail.com>
2024-02-15 16:16:54 +0100

    Update SignIn.js

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,36 +1,58 @@
-import React, { useEffect, useState } from "react";
-import {auth,provider} from "./config";
-import {signInWithPopup} from "firebase/auth";
-import Home from "./Home";
+import React, { useEffect, useState } from 'react'
+import { auth, provider } from './config'
+import { signInWithPopup } from 'firebase/auth'
+import Home from './Home'
 import '../CSS/SignIn_Up.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faFacebook , faGoogle } from "@fortawesome/free-brands-svg-icons"
-import {faUser,faLock,faChevronRight} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons'
+import {
+  faUser,
+  faLock,
+  faChevronRight,
+  faEnvelope
+} from '@fortawesome/free-solid-svg-icons'
 
-
-
-const SignIn = () => {
-  const [value,setValue] = useState('')
-    const handleClick =()=>{
-        signInWithPopup(auth,provider).then((data)=>{
-            setValue(data.user.email)
-            localStorage.setItem("email",data.user.email)
-        })
-    }
-
-    useEffect(()=>{
-        setValue(localStorage.getItem('email'))
+function SignIn () {
+  const [value, setValue] = useState('')
+  const handleClick = () => {
+    signInWithPopup(auth, provider).then(data => {
+      setValue(data.user.email)
+      localStorage.setItem('email', data.user.email)
     })
+  }
+
+  useEffect(() => {
+    setValue(localStorage.getItem('email'))
+  })
 
   return (
-  
- <div>
+    <div>
       <div className='container'>
         <div className='screen'>
           <div className='screen__content'>
             <form className='login'>
               <div className='signIn'>
-                <h1>Sign In</h1>
+                <h1>Welcome Back!</h1>
+              </div>
+              <div className='sso_text'>
+                <h2>Sign in to your account with : </h2>
+              </div>
+              <p>
+              <a className='twitter-before'>
+                <div className='google'>
+                  <img className="logo" src='https://reviews.rater8.com/images/google_logo_lg.png?v=1a839589-31f6-493f-9e87-ae99fb4065fb' alt='google'/>
+                </div>
+              </a>
+              {value ? (
+                <Home />
+              ) : (
+                <button className='twitter' onClick={handleClick}>
+                  <strong>Signin With Google</strong>
+                </button>
+              )}
+              </p>
+              <div className='sso_text_Or'>
+                <h2>OR: </h2>
               </div>
               <div className='login__field'>
                 <FontAwesomeIcon icon={faUser} />
@@ -54,21 +76,10 @@ const SignIn = () => {
                   <FontAwesomeIcon icon={faChevronRight} />
                 </div>
               </button>
-              <div className='sso_text'>
-                <h2>Or sign in with:</h2>
+              <div className='create'>
+                <a className='signup' href="/signup">Create an account</a>
               </div>
-              <p>
-              <a className='twitter-before'>
-                <FontAwesomeIcon icon={faGoogle} />
-              </a>
-              {value ? (
-                <Home />
-              ) : (
-                <button className='twitter' onClick={handleClick}>
-                  Signin With Google
-                </button>
-              )}
-              </p>
+             
             </form>
             <div className='social-login'>
               <div className='social-icons'>
@@ -87,8 +98,6 @@ const SignIn = () => {
         </div>
       </div>
     </div>
-
-  );
+  )
 }
-
-export default SignIn;
+export default SignIn
