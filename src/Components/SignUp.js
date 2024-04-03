b58bf8457e45201a9ed2246395462adcecd2c36f
Zeineb Hachaichi <74151518+zeineb23@users.noreply.github.com>
2024-04-03 02:41:46 +0100

    loginandpassword

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -3,18 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "../CSS/Login.css";
 import LoginIcon from '@mui/icons-material/Login';
 import { Navigate } from 'react-router-dom'; // Import Navigate for redirection
-import { signInWithPopup } from "firebase/auth";
+import { signInWithPopup, createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, provider } from "./config";
+
 const strengthLabels = ["Weak", "Medium", "Strong"];
 
 class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      email: '',
       password: '',
       strength: '',
       value: '',
-      redirectToSignIn: false // State to handle redirection to sign in
+      redirectToSignIn: false ,// State to handle redirection to sign in
+      redirectToHome: false // State to handle redirection to sign in
     };
   }
 
@@ -32,8 +35,24 @@ class SignUp extends Component {
     signInWithPopup(auth, provider).then((data) => {
       this.setState({ value: data.user.email });
       localStorage.setItem("email", data.user.email);
+      this.setState({ redirectToHome: true });
     });
   };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { email, password } = this.state;
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(userCredential);
+      const user = userCredential.user;
+      localStorage.setItem('user', JSON.stringify(user));
+      this.setState({ redirectToHome: true }); // Update state to trigger redirection
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
 
   handleChange = (event) => {
     const password = event.target.value;
@@ -46,11 +65,15 @@ class SignUp extends Component {
   };
 
   render() {
-    const { password, strength, redirectToSignIn } = this.state;
+    const { email, password, strength, redirectToSignIn , redirectToHome} = this.state;
     const strengthClassName = strength ? strength.toLowerCase() : '';
 
     if (redirectToSignIn) {
       return <Navigate to="/signin" />;
+    }
+  
+    if (redirectToHome) {
+      return <Navigate to="/" />;
     }
 
     return (
@@ -63,12 +86,26 @@ class SignUp extends Component {
             <h6 className='signin-msg'>Sign Up with Google</h6>
            
             <button className="google" onClick={this.handleGoogleLogin}>
-              <img src="https://d172mw7nx82lso.cloudfront.net/assets/landing/auth/google-d33f9eb20af60f124ea3de0def9116700064e558db8a63275354162d46ae09cb.png" width="140px" height="40px" />
+              <img src="https://d172mw7nx82lso.cloudfront.net/assets/landing/auth/google-d33f9eb20af60f124ea3de0def9116700064e558db8a63275354162d46ae09cb.png" width="140px" height="40px" alt="Google" />
             </button>
-            <br/>
+
+            <form onSubmit={this.handleSubmit} className="form"> 
+              <input type="email" placeholder="Username" value={email} onChange={(e) => this.setState({ email: e.target.value })} /> 
+              <input 
+                type="password" 
+                placeholder="Password"
+                value={password}
+                onChange={this.handleChange}
+              />
+              <div className={`strength ${strengthClassName}`}>
+                {strength && `${strength} password`}
+              </div>
+              <button type="submit">Sign Up</button> 
+            </form>
+
             <div className="footer"> 
               Have an account? 
-              <button className="sign" onClick={this.handleSignInRedirect}>Sign in</button>
+              <button  onClick={this.handleSignInRedirect}>Sign in</button>
               <br />
               <br />
             </div>
